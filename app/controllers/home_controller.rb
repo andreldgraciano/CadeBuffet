c54171cb13992b9e5699818bd71512820f4d88ac
AndreDias <dedasgdias@gmail.com>
2024-04-24 11:45:44 -0300

    melhorias home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,11 @@
 class HomeController < ApplicationController
   before_action :authenticate_buffet_profile!, only: [:buffet_profile]
-  before_action :authenticate_client!, only: [:client]#, unless: :buffet_profile_signed_in?
+  before_action :authenticate_client!, only: [:client]
 
   def index
     if buffet_profile_signed_in?
-      @buffet_profile = current_buffet_profile
-      if Buffet.exists?(buffet_profile_id: @buffet_profile.id)
-        buffet = Buffet.find_by(buffet_profile_id: @buffet_profile.id)
-        redirect_to buffet_path(buffet)
-      else
-        flash[:notice] = 'Cadastre seu biffet!'
-        redirect_to new_buffet_path
-      end
-    else client_signed_in?
+      redirect_to_buffet_or_create
+    elsif client_signed_in?
       redirect_to(buffets_path)
     end
   end
@@ -26,4 +19,15 @@ class HomeController < ApplicationController
 
   private
 
+  def redirect_to_buffet_or_create
+    @buffet_profile = current_buffet_profile
+    buffet = Buffet.find_by(buffet_profile_id: @buffet_profile.id)
+
+    if buffet
+      redirect_to buffet_path(buffet)
+    else
+      flash[:notice] = 'Cadastre seu buffet!'
+      redirect_to new_buffet_path
+    end
+  end
 end
