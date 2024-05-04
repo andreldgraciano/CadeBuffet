7ea659826f0255b6b81484cb72dd2bd2caafd20a
AndreDias <dedasgdias@gmail.com>
2024-05-04 20:16:49 -0300

    validates em event.rb, mudando campo para select no form event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :buffet
+
+  validates :name, :description, :min_people, :max_peaple, :duration, :menu, :address, :alcoholic_drink, :decoration, :parking, presence: true
 end
